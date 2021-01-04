05a989b77b9f19238a3cd4f6759516808164f321
iHoney725 <60086881+ohmyjesus@users.noreply.github.com>
2021-01-04 10:19:39 +0800

    clark变化

---
+++ clark_1_2s_3s.m
@@ -0,0 +1,7 @@
+function [Ua,Ub,Uc] = clark_1_2s_3s(Ualpha,Ubelta)
+
+          % 2s/3s±ä»»  N3/N2 = 2/3   ÇÒ  ia + ib + ic = 0 
+Ua = Ualpha;
+Ub=-1/2*Ualpha+sqrt(3)/2*Ubelta;
+Uc=-1/2*Ualpha-sqrt(3)/2*Ubelta;
+end
