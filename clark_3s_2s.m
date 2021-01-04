9045db1fec8927d82bf0aae8f3d3bef7a2c65cd8
iHoney725 <60086881+ohmyjesus@users.noreply.github.com>
2021-01-04 10:22:42 +0800

    clark坐标变换

--- clark_3s_2s.m
+++ clark_3s_2s.m
@@ -1,5 +1,5 @@
 function [Ualpha,Ubelta] = clark_3s_2s(Ua,Ub,Uc)
 K = sqrt(2/3);
-Ualpha=K*(Ua-1/2*Ub-1/2*Uc);               % 3s/2s�任  N3/N2 = 2/3   ��  ia + ib + ic = 0 
+Ualpha=K*(Ua-1/2*Ub-1/2*Uc);               % 3s/2s±ä»»  N3/N2 = 2/3   ÇÒ  ia + ib + ic = 0 
 Ubelta=K*(sqrt(3)/2*Ub-sqrt(3)/2*Uc);
-end
+end 
