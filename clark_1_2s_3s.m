973354120b47256ed62edab068735001138fc07b
iHoney725 <60086881+ohmyjesus@users.noreply.github.com>
2021-01-04 10:22:21 +0800

    clark坐标变换

--- clark_1_2s_3s.m
+++ clark_1_2s_3s.m
@@ -1,7 +1,7 @@
 function [Ua,Ub,Uc] = clark_1_2s_3s(Ualpha,Ubelta)
 
-          % 2s/3s�任  N3/N2 = 2/3   ��  ia + ib + ic = 0 
+          % 2s/3s±ä»»  N3/N2 = 2/3   ÇÒ  ia + ib + ic = 0 
 Ua = Ualpha;
-Ub=-1/2*Ualpha+sqrt(3)/2*Ubelta;
+Ub=-1/2*Ualpha+sqrt(3)/2*Ubelta; 
 Uc=-1/2*Ualpha-sqrt(3)/2*Ubelta;
 end
